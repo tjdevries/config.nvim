5740b4adee66d9d4d9ae2c2859b6ada490c6a274
TJ DeVries <devries.timothyj@gmail.com>
2024-11-13 23:06:21 -0500

    fix: actually guard php require

--- after/plugin/php.lua
+++ after/plugin/php.lua
@@ -2,11 +2,9 @@ if not pcall(require, "php") then
   return
 end
 
--- require("php").setup {
---   -- lsp = require "tj.lsp",
--- }
+require("php").setup {}
 
 vim.keymap.set("n", "<space>pa", function()
-  package.loaded["php.artisan"] = nil
+  R "php"
   require("php.artisan").telescope_select_artisan()
 end)
