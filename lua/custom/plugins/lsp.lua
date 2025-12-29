85172d183c18968c6995ab7f9346a0551d47948c
TJ DeVries <devries.timothyj@gmail.com>
2025-12-28 22:07:35 -0500

    wip: workin on some typescript improvements from dillon

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -24,6 +24,19 @@ return {
       { "https://git.sr.ht/~whynothugo/lsp_lines.nvim" },
 
       -- { "elixir-tools/elixir-tools.nvim" },
+      {
+        "dmmulroy/tsc.nvim",
+        config = function()
+          require("tsc").setup {
+            run_as_monorepo = true,
+          }
+        end,
+      },
+
+      {
+        "pmizio/typescript-tools.nvim",
+        dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+      },
 
       -- Autoformatting
       "stevearc/conform.nvim",
@@ -96,11 +109,11 @@ return {
         --     documentFormattingProvider = false,
         --   },
         -- },
-        vtsls = {
-          server_capabilities = {
-            documentFormattingProvider = false,
-          },
-        },
+        -- vtsls = {
+        --   server_capabilities = {
+        --     documentFormattingProvider = false,
+        --   },
+        -- },
         -- denols = true,
         jsonls = {
           server_capabilities = {
@@ -308,6 +321,65 @@ return {
           end
         end,
       })
+
+      -- require("typescript-tools").setup {
+      --   -- on_attach = on_attach,
+      --   settings = {
+      --     tsserver_plugins = {
+      --       "effect-language-service",
+      --     },
+      --     tsserver_file_preferences = {
+      --       includeInlayParameterNameHints = "all",
+      --       includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+      --       includeInlayFunctionParameterTypeHints = true,
+      --       includeInlayVariableTypeHints = true,
+      --       includeInlayPropertyDeclarationTypeHints = true,
+      --       includeInlayFunctionLikeReturnTypeHints = true,
+      --       includeInlayEnumMemberValueHints = true,
+      --     },
+      --   },
+      -- }
+
+      require("typescript-tools").setup {
+        -- on_attach = function(client, buffer_number)
+        --   require("twoslash-queries").attach(client, buffer_number)
+        -- end,
+        settings = {
+          -- tsserver_path = "~/.bun/bin/tsgo",
+          -- Performance: separate diagnostic server for large projects
+          separate_diagnostic_server = true,
+          -- When to publish diagnostics
+          publish_diagnostic_on = "insert_leave",
+          -- JSX auto-closing tags
+          jsx_close_tag = {
+            enable = true,
+            filetypes = { "javascriptreact", "typescriptreact" },
+          },
+          tsserver_file_preferences = {
+            includeInlayParameterNameHints = "all",
+            includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+            includeInlayVariableTypeHints = true,
+            includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+            includeInlayPropertyDeclarationTypeHints = true,
+            includeInlayFunctionParameterTypeHints = true,
+            includeInlayEnumMemberValueHints = true,
+            includeInlayFunctionLikeReturnTypeHints = true,
+            -- Enable auto imports
+            includeCompletionsForModuleExports = true,
+            includeCompletionsForImportStatements = true,
+          },
+
+          tsserver_format_options = {
+            insertSpaceAfterOpeningAndBeforeClosingEmptyBraces = true,
+            semicolons = "insert",
+          },
+          complete_function_calls = true,
+          include_completions_with_insert_text = true,
+          code_lens = "off",
+          disable_member_code_lens = true,
+          tsserver_max_memory = 12288,
+        },
+      }
 
       require("custom.autoformat").setup()
 
